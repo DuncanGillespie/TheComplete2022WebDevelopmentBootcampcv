13cc5a0fba309279d9226214eaf85e387c6b4a88
Duncan Gillespie <dgilles3@myune.edu.au>
2022-10-06 17:38:51 +1000

    starting dicee game

---
+++ javascript3thedom/index.js
@@ -0,0 +1,51 @@
+// document.getElementById("demo").innerHTML = "Updated Demo!";
+
+// html 
+// document.firstElementChild;
+
+// head 
+// document.firstElementChild.firstElementChild;
+
+// body
+// document.firstElementChild.lastElementChild;
+
+// title 
+// document.firstElementChild.lastElementChild.firstElementChild;
+// document.firstElementChild.lastElementChild.firstElementChild.innerHTML = "Goodbye";
+// document.firstElementChild.lastElementChild.firstElementChild.style.color = "red";
+
+// var heading = document.firstElementChild.lastElementChild.firstElementChild;
+
+// heading.innerHTML = "innerHTML";
+
+// Third list item to forth
+//console.log(document.getElementsByTagName("li"));
+// document.getElementsByTagName("li")[2].innerHTML = "forth";
+
+// // query selector class with multiple entries
+// document.querySelectorAll(".item")[1].innerHTML = "fifth";
+
+// // change the color of the link in the class
+// document.querySelectorAll(".item a")[0].style.color = "red";
+
+// change background of button
+// document.querySelector("button").style.backgroundColor = "yellow";
+
+// add class to item
+// document.querySelector("button").classList.add("invisible"); 
+// document.querySelector("button").classList.remove("invisible");
+// document.querySelector("button").classList.toggle("invisible");
+// document.querySelector("button").classList.toggle("invisible");
+
+// add the huge class to the title
+// document.querySelector("h1").classList.add("huge");
+
+// change the html as well as the text content
+// console.log(document.getElementById("title").textContent);
+// document.getElementById("title").innerHTML = "<em>Second Title</em>";
+
+// document attributes 
+// console.log(document.querySelector("a").attributes);
+// document.querySelector("a").getAttribute("href");
+// document.querySelector("a").setAttribute("href", "https://www.bing.com");
+
